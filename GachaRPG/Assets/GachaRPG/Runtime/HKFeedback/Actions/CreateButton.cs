550b468975e98147cfa806a8418f695c16acb1f1
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2026-04-10 23:29:44 +0900

    HKFeedback用の新しいアクションとメタデータファイルを作成

---
+++ GachaRPG/Assets/GachaRPG/Runtime/HKFeedback/Actions/CreateButton.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using HKFeedback;
+
+namespace GachaRPG.HKFeedback.Actions
+{
+    [Serializable]
+    public sealed class CreateButton<TContext> : IFeedback<TContext>
+    {
+        public UniTask PlayAsync(TContext context, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
