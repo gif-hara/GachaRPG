36d7814142a285331f2ddaf3d33157d8ffbe85d2
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2026-04-11 00:14:08 +0900

    CreateListButtonにボタン作成機能を実装

--- GachaRPG/Assets/GachaRPG/Runtime/HKFeedback/Actions/CreateListButton.cs
+++ GachaRPG/Assets/GachaRPG/Runtime/HKFeedback/Actions/CreateListButton.cs
@@ -2,15 +2,21 @@ using System;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using HKFeedback;
+using HKFeedback.Extensions;
+using UnityEngine;
 
 namespace GachaRPG.HKFeedback.Actions
 {
     [Serializable]
-    public sealed class CreateListButton<TContext> : IFeedback<TContext>
+    public sealed class CreateListButton<TContext> : IFeedback<TContext> where TContext : IProvider<UIViewList>
     {
+        [SerializeReference, SubclassSelector]
+        private IFeedback<UIElementButton>[] onCreatedFeedbacks;
+
         public UniTask PlayAsync(TContext context, CancellationToken cancellationToken)
         {
-            throw new System.NotImplementedException();
+            var button = context.Provide().CreateButton();
+            return onCreatedFeedbacks.PlayAsync(button, cancellationToken);
         }
     }
 }
