ed4e2c070eafa7e73f5d562d45cf027cbecf1a77
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2026-04-11 07:33:16 +0900

    BeginFlowAsyncアクションを新規作成し、フローを開始する機能を追加

---
+++ GachaRPG/Assets/GachaRPG/Runtime/HKFeedback/Actions/BeginFlowAsync.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using HKFeedback;
+using UnityEngine;
+
+namespace GachaRPG.HKFeedback.Actions
+{
+    [Serializable]
+    public sealed class BeginFlowAsync<TContext> : IFeedback<TContext> where TContext : IProvider<MainSceneController>
+    {
+        [SerializeField]
+        private MainSceneFlow flow;
+
+        public UniTask PlayAsync(TContext context, CancellationToken cancellationToken)
+        {
+            return context.Provide().BeginFlowAsync(flow);
+        }
+    }
+}
