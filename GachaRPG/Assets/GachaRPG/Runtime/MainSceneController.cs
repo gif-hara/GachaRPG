a08f64e3da9667342e69b14835eeb602860fc333
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2026-04-11 00:14:02 +0900

    MainSceneControllerにUIViewListの提供機能を追加

--- GachaRPG/Assets/GachaRPG/Runtime/MainSceneController.cs
+++ GachaRPG/Assets/GachaRPG/Runtime/MainSceneController.cs
@@ -1,8 +1,22 @@
+using Cysharp.Threading.Tasks;
+using HKFeedback;
 using UnityEngine;
 
 namespace GachaRPG
 {
-    public class MainSceneController : MonoBehaviour
+    public class MainSceneController : MonoBehaviour, IProvider<UIViewList>
     {
+        [SerializeField]
+        private MainSceneFlow entryFlow;
+
+        [SerializeField]
+        private UIViewList uiViewList;
+
+        UIViewList IProvider<UIViewList>.Provide() => uiViewList;
+
+        private void Start()
+        {
+            entryFlow.PlayAsync(this, destroyCancellationToken).Forget();
+        }
     }
 }
