2a803f826ce31d52cdd0a1a5ebcd7f7a8950ae88
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2026-04-11 00:13:22 +0900

    UIViewListクラスを新規作成し、ボタンの管理機能を追加

---
+++ GachaRPG/Assets/GachaRPG/Runtime/UIViewList.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GachaRPG
+{
+    public class UIViewList : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform buttonParent;
+
+        [SerializeField]
+        private UIElementButton buttonPrefab;
+
+        private readonly List<UIElementButton> buttons = new();
+
+        public void Clear()
+        {
+            foreach (var button in buttons)
+            {
+                Destroy(button.gameObject);
+            }
+            buttons.Clear();
+        }
+
+        public UIElementButton CreateButton()
+        {
+            var button = Instantiate(buttonPrefab, buttonParent);
+            buttons.Add(button);
+            return button;
+        }
+    }
+}
