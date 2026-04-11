b5cfec5c6bfbcc35db15a11678a909af732c8b5c
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2026-04-11 12:54:06 +0900

    GachaElementクラスを新規作成し、要素の重み付きランダム選択機能を追加

---
+++ GachaRPG/Assets/GachaRPG/Runtime/GachaElement.cs
@@ -0,0 +1,25 @@
+using System;
+using HK;
+using UnityEngine;
+
+namespace GachaRPG
+{
+    [CreateAssetMenu(fileName = "GachaElement", menuName = "GachaRPG/Gacha Element")]
+    public class GachaElement : ScriptableObject
+    {
+        [SerializeField]
+        private Element[] elements;
+
+        public PassiveSkill Lottery() => elements.Lottery(x => x.Weight).PassiveSkill;
+
+        [Serializable]
+        public class Element
+        {
+            [field: SerializeField]
+            public PassiveSkill PassiveSkill { get; private set; }
+
+            [field: SerializeField]
+            public int Weight { get; private set; }
+        }
+    }
+}
