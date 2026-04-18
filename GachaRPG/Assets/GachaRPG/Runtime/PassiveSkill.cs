1d11627083a680e175b304951f1b84fbba786a39
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2026-04-18 17:15:36 +0900

    PassiveSkillクラスにSerializable属性を追加し、DictionaryListクラスを定義

--- GachaRPG/Assets/GachaRPG/Runtime/PassiveSkill.cs
+++ GachaRPG/Assets/GachaRPG/Runtime/PassiveSkill.cs
@@ -1,4 +1,6 @@
+using System;
 using Cysharp.Threading.Tasks;
+using HK;
 using HKFeedback;
 using HKFeedback.Extensions;
 using UnityEngine;
@@ -17,6 +19,14 @@ namespace GachaRPG
         public void Activate(PassiveSkillContext context)
         {
             actions.PlayAsync(context, default).Forget();
+        }
+
+        [Serializable]
+        public sealed class DictionaryList : DictionaryList<string, PassiveSkill>
+        {
+            public DictionaryList() : base(element => element.name)
+            {
+            }
         }
     }
 }
