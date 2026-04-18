397bf04b84420024e2fd9e079a0ccea024ee8f70
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2026-04-18 17:19:00 +0900

    GameRuleクラスにPassiveSkillsプロパティを追加し、Setupメソッドを実装

--- GachaRPG/Assets/GachaRPG/Runtime/GameRule.cs
+++ GachaRPG/Assets/GachaRPG/Runtime/GameRule.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
 namespace GachaRPG
@@ -10,5 +12,27 @@ namespace GachaRPG
 
         [field: SerializeField]
         public int GachaElementSize { get; private set; } = 3;
+
+        [field: SerializeField]
+        public PassiveSkill.DictionaryList PassiveSkills { get; private set; } = null!;
+
+        [ContextMenu("Setup")]
+        private void Setup()
+        {
+            var passiveSkills = AssetDatabase.FindAssets("t:PassiveSkill", new[] { "Assets/GachaRPG/Database/PassiveSkill" })
+                .Select(guid => AssetDatabase.LoadAssetAtPath<PassiveSkill>(AssetDatabase.GUIDToAssetPath(guid)))
+                .ToArray();
+            if (PassiveSkills == null)
+            {
+                PassiveSkills = new PassiveSkill.DictionaryList();
+            }
+            foreach (var passiveSkill in passiveSkills)
+            {
+                if (!PassiveSkills.ContainsKey(passiveSkill.name))
+                {
+                    PassiveSkills.Add(passiveSkill);
+                }
+            }
+        }
     }
 }
