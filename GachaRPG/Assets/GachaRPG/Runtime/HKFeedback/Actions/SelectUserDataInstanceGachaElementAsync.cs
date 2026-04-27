7e4029c106930aa1459accc6d94a471a3dd08b22
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2026-04-27 19:53:22 +0900

    SelectUserDataInstanceGachaElementAsyncにフィードバック処理を追加し、選択結果の条件分岐を実装

--- GachaRPG/Assets/GachaRPG/Runtime/HKFeedback/Actions/SelectUserDataInstanceGachaElementAsync.cs
+++ GachaRPG/Assets/GachaRPG/Runtime/HKFeedback/Actions/SelectUserDataInstanceGachaElementAsync.cs
@@ -9,13 +9,33 @@ namespace GachaRPG.HKFeedback.Actions
     [Serializable]
     public sealed class SelectUserDataInstanceGachaElementAsync<TContext> : IFeedback<TContext> where TContext : IProvider<MainSceneContext>
     {
+        [SerializeReference, SubclassSelector]
+        private IFeedback<TContext>[] selectFeedbacks;
+
+        [SerializeReference, SubclassSelector]
+        private IFeedback<TContext>[] cancelFeedbacks;
+
         public async UniTask PlayAsync(TContext context, CancellationToken cancellationToken)
         {
             try
             {
                 var mainSceneContext = context.Provide();
                 var result = await mainSceneContext.UIViewList.SelectInstanceGachaElementAsync(mainSceneContext.UserData.GachaElements, cancellationToken);
-                mainSceneContext.MainSceneController.SelectedInstanceGachaElement = result;
+                if (!result.isCancel)
+                {
+                    mainSceneContext.MainSceneController.SelectedInstanceGachaElement = result.instanceGachaElement;
+                    foreach (var feedback in selectFeedbacks)
+                    {
+                        await feedback.PlayAsync(context, cancellationToken);
+                    }
+                }
+                else
+                {
+                    foreach (var feedback in cancelFeedbacks)
+                    {
+                        await feedback.PlayAsync(context, cancellationToken);
+                    }
+                }
             }
             catch (OperationCanceledException)
             {
