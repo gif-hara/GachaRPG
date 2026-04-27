bfebc39f0ed6ac520d8edb4fe6e6e707d81c378b
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2026-04-27 20:49:51 +0900

    GachaResultクラスにInstanceIdプロパティを追加し、コンストラクタを修正しました。

--- GachaRPG/Assets/GachaRPG/Runtime/GachaResult.cs
+++ GachaRPG/Assets/GachaRPG/Runtime/GachaResult.cs
@@ -1,20 +1,31 @@
 using System.Collections.Generic;
 using System.Linq;
+using HK;
 
 namespace GachaRPG
 {
     public class GachaResult
     {
+        public int InstanceId { get; private set; }
+
         public List<InstancePassiveSkill> PassiveSkills { get; }
 
-        public GachaResult(List<InstancePassiveSkill> passiveSkills)
+        public GachaResult(int instanceId, List<InstancePassiveSkill> passiveSkills)
         {
+            InstanceId = instanceId;
             PassiveSkills = passiveSkills;
         }
 
         public override string ToString()
         {
-            return $"GachaResult: {string.Join(", ", PassiveSkills.Select(x => x.PassiveSkill.SkillName))}";
+            return $"GachaResult (InstanceId: {InstanceId}): {string.Join(", ", PassiveSkills.Select(x => x.PassiveSkill.SkillName))}";
+        }
+
+        public sealed class DictionaryList : DictionaryList<int, GachaResult>
+        {
+            public DictionaryList() : base(element => element.InstanceId)
+            {
+            }
         }
     }
 }
