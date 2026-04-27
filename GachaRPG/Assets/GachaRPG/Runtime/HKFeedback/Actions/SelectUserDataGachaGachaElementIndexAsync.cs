b52bbe5a8ac27567b7ef3b748b8b6277d61ba4cd
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2026-04-27 19:47:46 +0900

    SelectUserDataGachaIndexAsyncを削除し、SelectUserDataGachaGachaElementIndexAsyncを追加

---
+++ GachaRPG/Assets/GachaRPG/Runtime/HKFeedback/Actions/SelectUserDataGachaGachaElementIndexAsync.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using HKFeedback;
+using UnityEngine;
+
+namespace GachaRPG.HKFeedback.Actions
+{
+    [Serializable]
+    public sealed class SelectUserDataGachaGachaElementIndexAsync<TContext> : IFeedback<TContext> where TContext : IProvider<MainSceneContext>
+    {
+        [SerializeReference, SubclassSelector]
+        private IFeedback<TContext>[] selectFeedbacks;
+
+        [SerializeReference, SubclassSelector]
+        private IFeedback<TContext>[] cancelFeedbacks;
+
+        public async UniTask PlayAsync(TContext context, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var mainSceneContext = context.Provide();
+                var result = await mainSceneContext.UIViewList.SelectGachaElementIndexAsync(mainSceneContext.UserData.Gacha, cancellationToken);
+                if (!result.isCancel)
+                {
+                    mainSceneContext.MainSceneController.SelectedGacha_GachaElementIndex = result.index;
+                    foreach (var feedback in selectFeedbacks)
+                    {
+                        await feedback.PlayAsync(context, cancellationToken);
+                    }
+                }
+                else
+                {
+                    foreach (var feedback in cancelFeedbacks)
+                    {
+                        await feedback.PlayAsync(context, cancellationToken);
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                Debug.Log("Selection cancelled.");
+            }
+            catch (InvalidOperationException)
+            {
+                Debug.Log("No buttons available for selection.");
+            }
+        }
+    }
+}
