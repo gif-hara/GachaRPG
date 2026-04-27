deab297f8592b73c193a9858f0c033a9b971791c
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2026-04-27 20:49:34 +0900

    SelectGachaResultAsyncメソッドを追加し、条件に基づいてガチャ結果ボタンを生成する処理を実装

--- GachaRPG/Assets/GachaRPG/Runtime/Extensions.UIViewList.cs
+++ GachaRPG/Assets/GachaRPG/Runtime/Extensions.UIViewList.cs
@@ -69,5 +69,37 @@ namespace GachaRPG
                     .ContinueWith(result => (elementButtons[result.result1.winArgumentIndex].instanceGachaElement, isCancel: result.winArgumentIndex == 1));
             }
         }
+
+        public static UniTask<(GachaResult gachaResult, bool isCancel)> SelectGachaResultAsync(this UIViewList uiViewList, GachaResult.DictionaryList gachaResults, ICondition<GachaResult> condition, CancellationToken cancellationToken)
+        {
+            var resultButtons = new List<(UIElementButton button, GachaResult gachaResult)>();
+            foreach (var gachaResult in gachaResults.List)
+            {
+                if (!condition.EvaluateSafe(gachaResult))
+                {
+                    continue;
+                }
+                var button = uiViewList.CreateButton();
+                button.ButtonText.SetText($"[{gachaResult.InstanceId}]");
+                resultButtons.Add((button, gachaResult));
+            }
+
+            var cancelButton = uiViewList.CreateButton();
+            cancelButton.ButtonText.SetText("戻る");
+
+            if (resultButtons.Count == 0)
+            {
+                return cancelButton.OnClickAsObservable().FirstAsync(cancellationToken).AsUniTask()
+                    .ContinueWith(_ => (gachaResult: (GachaResult)null, isCancel: true));
+            }
+            else
+            {
+                var resultButtonTask = UniTask.WhenAny(resultButtons.Select(x => x.button.OnClickAsObservable().FirstAsync(cancellationToken).AsUniTask()));
+                var cancelTask = cancelButton.OnClickAsObservable().FirstAsync(cancellationToken).AsUniTask();
+
+                return UniTask.WhenAny(resultButtonTask, cancelTask)
+                    .ContinueWith(result => (resultButtons[result.result1.winArgumentIndex].gachaResult, isCancel: result.winArgumentIndex == 1));
+            }
+        }
     }
 }
