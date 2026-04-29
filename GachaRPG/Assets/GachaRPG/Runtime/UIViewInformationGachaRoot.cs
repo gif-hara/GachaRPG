5dd2468bf54152956299891ef4b1159a4e794121
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2026-04-29 11:30:13 +0900

    UIViewInformationGachaRootクラスにユーザーデータを適用する機能を追加

--- GachaRPG/Assets/GachaRPG/Runtime/UIViewInformationGachaRoot.cs
+++ GachaRPG/Assets/GachaRPG/Runtime/UIViewInformationGachaRoot.cs
@@ -1,9 +1,47 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
 namespace GachaRPG
 {
     public class UIViewInformationGachaRoot : UIViewInformationBase
     {
+        [SerializeField]
+        private Transform uiElementInstanceGachaElementParent;
+
+        [SerializeField]
+        private UIElementInstanceGachaElement uiElementInstanceGachaElement;
+
+        private List<UIElementInstanceGachaElement> uiElementInstanceGachaElements = new();
+
+        private UserData userData;
+
+        private void OnEnable()
+        {
+            UpdateView();
+        }
+
         public override void Initialize(MainSceneContext context)
         {
+            userData = context.UserData;
+        }
+
+        private void UpdateView()
+        {
+            Assert.IsNotNull(userData, $"{nameof(userData)} is not set.");
+
+            foreach (var uiElementInstanceGachaElement in uiElementInstanceGachaElements)
+            {
+                Destroy(uiElementInstanceGachaElement.gameObject);
+            }
+            uiElementInstanceGachaElements.Clear();
+
+            foreach (var instanceGachaElement in userData.Gacha.Elements)
+            {
+                var instance = Instantiate(uiElementInstanceGachaElement, uiElementInstanceGachaElementParent);
+                instance.Apply(instanceGachaElement);
+                uiElementInstanceGachaElements.Add(instance);
+            }
         }
     }
 }
